6c6f5a8128a27e581f52cf887f886ab9520b7758
Rob Giusti <rob@lvl6.com>
2014-06-05 17:04:52 -0700

    Added images for Quest Complete Screen

---
+++ Assets/Code/MobSquad/City/Managers/MSLocalization.cs
@@ -0,0 +1,41 @@
+﻿using GoogleFu;
+using UnityEngine;
+
+public enum Language {EN, FR, JA};
+
+public class MSLocalization : MonoBehaviour
+{
+	public static Language language = Language.FR;
+	
+	public static string GetString(Sheet1.rowIds rowId)
+	{
+		return Sheet1.Instance.GetRow(rowId).GetStringData(language.ToString());
+	}
+
+	public void ChangeLanguage(Language language)
+	{
+		MSLocalization.language = language;
+		foreach (MSLocalizedLabel label in GameObject.FindObjectsOfType(typeof(MSLocalizedLabel))) 
+		{
+			label.Refresh();
+		}
+	}
+
+	[ContextMenu ("Set Language to English")]
+	public void English()
+	{
+		ChangeLanguage(Language.EN);
+	}
+
+	[ContextMenu ("Set Language to French")]
+	public void French()
+	{
+		ChangeLanguage(Language.FR);
+	}
+
+	[ContextMenu ("Set Language to Japanese")]
+	public void Japanese()
+	{
+		ChangeLanguage(Language.JA);
+	}
+}
---
+++ Assets/Code/MobSquad/City/UI/MSLocalizedLabel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleFu;
+
+[RequireComponent (typeof (UILabel))]
+public class MSLocalizedLabel : MonoBehaviour 
+{
+	public Sheet1.rowIds stringId;
+
+	UILabel label;
+
+	void Awake()
+	{
+		label = GetComponent<UILabel>();
+	}
+
+	void OnEnable()
+	{
+		Refresh();
+	}
+
+	public void Refresh()
+	{
+		label.text = MSLocalization.GetString(stringId);
+	}
+}
