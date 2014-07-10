0f09f7a576d8c99e4a8a2be23192fefb804f3958
Inlight2 <kenny@lvl6.com>
2014-07-10 13:01:23 -0700

    merge to master a tab may work

---
+++ Assets/Code/MobSquad/City/UI/ShopMenu/MSShopBuildingCard.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MSShopBuildingCard : MonoBehaviour {
+
+	[SerializeField]
+	UISprite buildingIcon;
+
+	[SerializeField]
+	UILabel title;
+	
+	[SerializeField]
+	UILabel description;
+
+	[SerializeField]
+	UISprite currency;
+	
+	[SerializeField]
+	UILabel cost;
+	
+	[SerializeField]
+	UILabel quantity;
+	
+	[SerializeField]
+	UILabel time;
+
+	[SerializeField]
+	UILabel requirement;
+
+	[SerializeField]
+	UISprite divider;
+
+	[SerializeField]
+	UISprite clock;
+
+	[SerializeField]
+	UILabel builtLabel;
+
+	UIButton button;
+
+	const string ACTIVE_BACKGROUND = "menusqaureactive";
+
+	const string INACTIVE_BACKGROUND = "menusqareinactive";
+
+	const string ACTIVE_DIVIDER = "activedividerline";
+
+	const string INACTIVE_DIVIDER = "inactivedividerline";
+
+	public enum State{
+		ACTIVE,
+		INACTIVE
+	}
+	
+	public State _state;
+	
+	public State state{
+		get{
+			return _state;
+		}
+		set{
+			_state = value;
+
+			if(value == State.ACTIVE){
+				Debug.Log("Active");
+				//button.normalSprite = ACTIVE_BACKGROUND;
+				divider.spriteName = ACTIVE_DIVIDER;
+
+				clock.enabled = true;
+				builtLabel.enabled = true;
+				time.enabled = true;
+				quantity.enabled = true;
+				cost.enabled = true;
+				currency.enabled = true;
+
+
+				requirement.enabled = false;
+			}else{
+
+				Debug.Log("Inactive");
+				//button.normalSprite = ACTIVE_BACKGROUND;
+				divider.spriteName = ACTIVE_DIVIDER;
+				
+				clock.enabled = false;
+				builtLabel.enabled = false;
+				time.enabled = false;
+				quantity.enabled = false;
+				cost.enabled = false;
+				currency.enabled = false;
+				
+				
+				requirement.enabled = true;
+			}
+		}
+	}
+
+	void awake(){
+		button = GetComponent<UIButton>();
+		_state = State.ACTIVE;
+	}
+
+	void Update(){
+		state = _state;
+	}
+}
---
+++ Assets/Code/MobSquad/City/UI/ShopMenu/MSTabButton.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MSTabButton : MonoBehaviour {
+
+	[SerializeField]
+	string iconWhite;
+
+	[SerializeField]
+	string iconBlue;
+
+	[SerializeField]
+	UISprite icon;
+
+	[SerializeField]
+	UILabel label;
+
+	[SerializeField]
+	Color textBlue;
+
+	enum Tab{
+		BUILDING,
+		FUNDS,
+		MOBSTERS
+	}
+
+	[SerializeField]
+	Tab buttonType;
+
+	UIButton button;
+	
+	UISprite tabSprite;
+
+	int startingDepth;
+
+	public static Action ClickTab;
+
+	const string TAB_PRESSED = "menuinactivetabpressed";
+
+	const string TAB_INACTIVE = "menuinactivetab";
+
+	const string TAB_ACTIVE = "menuactivetab";
+
+	void Awake(){
+		button = GetComponent<UIButton>();
+		tabSprite = GetComponent<UISprite>();
+		startingDepth = tabSprite.depth;
+		ClickTab += Deselect;
+	}
+
+	void ChangeDepthTo(int depth){
+		tabSprite.depth = depth;
+		label.depth = depth + 1;
+		icon.depth = depth + 1;
+	}
+
+	void Deselect(){
+		button.normalSprite = TAB_INACTIVE;
+		button.pressedSprite = TAB_PRESSED;
+		icon.spriteName = iconWhite;
+		label.color = Color.white;
+		ChangeDepthTo(startingDepth);
+	}
+
+	void OnClick(){
+		ClickTab();
+		button.normalSprite = TAB_ACTIVE;
+		button.pressedSprite = TAB_ACTIVE;
+		icon.spriteName = iconBlue;
+		label.color = textBlue;
+		ChangeDepthTo(startingDepth + 2);
+		Debug.Log(buttonType.ToString());
+	}
+}
