5a3891696253a91e871a061562adec5fcd568baa
Inlight2 <kenny@lvl6.com>
2014-09-19 19:05:56 -0700

    mapAtlasFonts

--- Assets/Code/MobSquad/Puzzle/UI/PZRetreatButton.cs
+++ Assets/Code/MobSquad/Puzzle/UI/PZRetreatButton.cs
@@ -7,14 +7,30 @@ public class PZRetreatButton : MonoBehaviour {
 	[SerializeField]
 	MSPopup retreatPopup;
 
-//	UIButton button;
+	UIButton button;
 //
-//	UISprite sprite;
+	UISprite sprite;
 
 	void Awake()
 	{
-//		button = GetComponent<UIButton>();
-//		sprite = GetComponent<UISprite>();
+		button = GetComponent<UIButton>();
+		sprite = GetComponent<UISprite>();
+	}
+
+	void OnEnable()
+	{
+		MSActionManager.Puzzle.OnTurnChange += TurnChange;
+	}
+
+	void OnDisable()
+	{
+		MSActionManager.Puzzle.OnTurnChange -= TurnChange;
+	}
+
+	void TurnChange(int turnsLeft)
+	{
+		button.enabled = turnsLeft > 0;
+		sprite.alpha = (button.enabled) ? 1 : 0;
 	}
 
 	void OnClick()
